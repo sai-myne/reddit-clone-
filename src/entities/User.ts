518243a0b8ba7f384f1beb28d3dbf1b0da9891c8
sai-myne <ahlusir@gmail.com>
2021-08-26 19:28:05 +0800

    Creating Posts & Subs

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,26 +1,23 @@
-import { IsEmail, Length} from "class-validator";
+import { IsEmail, Length } from "class-validator";
 import {
-  Entity,
-  PrimaryGeneratedColumn,
+  Entity as TOEntity,
   Column,
-  BaseEntity,
   Index,
-  CreateDateColumn,
-  UpdateDateColumn,
   BeforeInsert,
+  OneToMany,
 } from "typeorm";
-import bcrypt from 'bcrypt'
-import { classToPlain, Exclude } from "class-transformer";
+import bcrypt from "bcrypt";
+import { Exclude } from "class-transformer";
 
-@Entity("users")
-export class User extends BaseEntity {
+import Entity from "./Entity";
+import Post from "./Post";
+
+@TOEntity("users")
+export default class User extends Entity {
   constructor(user: Partial<User>) {
     super();
     Object.assign(this, user);
   }
-  @Exclude()
-  @PrimaryGeneratedColumn()
-  id: number;
 
   @Index()
   @IsEmail()
@@ -28,7 +25,7 @@ export class User extends BaseEntity {
   email: string;
 
   @Index()
-  @Length(3, 255, { message: 'Username must be at least 3 characters long'})
+  @Length(3, 255, { message: "Username must be at least 3 characters long" })
   @Column({ unique: true })
   username: string;
 
@@ -37,18 +34,11 @@ export class User extends BaseEntity {
   @Length(6, 255)
   password: string;
 
-  @CreateDateColumn()
-  createdAt: Date;
-
-  @UpdateDateColumn()
-  updatedAt: Date;
+  @OneToMany(() => Post, (post) => post.user)
+  posts: Post[];
 
   @BeforeInsert()
-  async hashPassword(){
-      this.password = await bcrypt.hash(this.password, 6)
-  }
-
-  toJSON(){
-      return classToPlain(this)
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 6);
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,12 @@ import morgan from "morgan";
 import cookieParser from 'cookie-parser'
 
 import authRoutes from './routes/auth'
+import postRoutes from './routes/post'
+import subRoutes from './routes/subs'
 import trim from './middleware/trim'
 
 const app = express()
+const PORT = process.env.PORT
 
 app.use(express.json())
 app.use(morgan('dev'))
@@ -15,9 +18,11 @@ app.use(trim)
 app.use(cookieParser())
 app.get('/', (_, res) => res.send('Hello World'))
 app.use('/api/auth', authRoutes)
+app.use('/api/posts', postRoutes)
+app.use('/api/subs', subRoutes)
 
-app.listen(5000, async () => {
-    console.log('Server running at http://localhost:5000')
+app.listen(PORT, async () => {
+    console.log(`Server running at http://localhost:${PORT}`)
 
     try{
         await createConnection()
